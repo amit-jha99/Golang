68557bdf9c697761b394fbd57c6720d9f0938089
amit-jha99 <amitjha121299@gmail.com>
2024-06-19 12:42:21 +0530

    003 folder added

---
+++ 003/main.go
@@ -0,0 +1,10 @@
+// Exercise: Create a program that puts the "Hello World!" string inside a string variable called "helloWorld"
+
+package main
+
+import "fmt"
+
+func main() {
+	helloWorld := "Hello World!"
+	fmt.Println(helloWorld)
+}
