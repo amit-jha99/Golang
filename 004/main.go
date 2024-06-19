44a9fcd8dc4cc8ae9e4e1dceb4d6e65cc2480c63
amit-jha99 <amitjha121299@gmail.com>
2024-06-19 12:47:46 +0530

    004 folder added

---
+++ 004/main.go
@@ -0,0 +1,14 @@
+// Exercise: Concatenate two string variables and print it's result
+// One variable should be called "helloWorld" and the other "itsmemario"
+// There should be a space between them
+
+package main
+
+import "fmt"
+
+func main() {
+
+	helloWorld := "Hello World!"
+	itsmemario := "it's me mario"
+	fmt.Println(helloWorld + " " + itsmemario)
+}
