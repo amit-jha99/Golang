53a07adbc0b85842bb58997171dc2788aa3790c0
amit-jha99 <amitjha121299@gmail.com>
2024-06-19 12:38:10 +0530

    002 folder added

---
+++ 002/main.go
@@ -0,0 +1,13 @@
+// Exercise: Create a program that shows your name, address and also has some comments in it
+// - Use the fmt package
+
+package main
+
+import "fmt"
+
+func main() {
+	//Name
+	fmt.Println("My name is :Amit Jha")
+	//Address
+	fmt.Println("My adderss is :Mumbai")
+}
